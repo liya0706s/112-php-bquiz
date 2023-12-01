3d6dc2dda84afed21ee03deb93df163a5d734f4e
Angie <liya0706s@gmail.com>
2023-12-01 09:50:36 +0800

    建立問卷管理後台頁面

--- q2/admin.php
+++ q2/admin.php
@@ -14,7 +14,30 @@
         <h1 class="text-center">問卷管理後台</h1>
     </header>
     <main class="container p-3">
-
+        <fieldset>
+            <legend>新增問卷</legend>
+            <form action="add_que.php" method="post">
+                <!-- 名稱 -->
+                <div class="d-flex">
+                    <div class="col-3 bg-light p-2">問卷名稱</div>
+                    <div class="col-6 p-2">
+                        <input type="text" name="subject" id="">
+                    </div>
+                </div>
+                <!-- 選項 -->
+                <div class="bg-light">
+                    <div class="p-2">
+                        <label for="">選項</label>
+                        <input type="text" name="opt[]">
+                        <input type="button" value="更多">
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <input type="submit" value="新增">
+                    <input type="reset" value="清空">
+                </div>
+            </form>
+        </fieldset>
 
 
     </main>
